53e88499499877695d6731623600a280c795b05a
Adarsh Roy <adarshroy.formal@gmail.com>
2024-11-02 05:37:53 +0530

    make git log one line and remove gitblame

--- lua/custom/plugins/vim-fugitive.lua
+++ lua/custom/plugins/vim-fugitive.lua
@@ -3,11 +3,12 @@ return {
   config = function()
     -- Keymaps for Vim Fugitive commands
     vim.keymap.set('n', '<leader>gs', ':Git<CR>', { desc = 'Git Status' })
-    vim.keymap.set('n', '<leader>gw', ':Gwrite<CR>', { desc = 'Git Stage' })
+    vim.keymap.set('n', '<leader>gw', ':Gwrite<CR>', { desc = 'Git Stage current file' })
+    vim.keymap.set('n', '<leader>ga.', ':Git add .<CR>', { desc = 'Git Add current directory' })
+    vim.keymap.set('n', '<leader>gau', ':Git add -u<CR>', { desc = 'Git Add updated files only' })
     vim.keymap.set('n', '<leader>gc', ':Git commit<CR>', { desc = 'Git Commit' })
     vim.keymap.set('n', '<leader>gp', ':Git push<CR>', { desc = 'Git Push' })
-    vim.keymap.set('n', '<leader>gl', ':Git log<CR>', { desc = 'Git Log' })
+    vim.keymap.set('n', '<leader>gl', ':Git log --oneline<CR>', { desc = 'Git Log (oneline)' })
     vim.keymap.set('n', '<leader>gd', ':Gdiffsplit<CR>', { desc = 'Git Diff' })
-    vim.keymap.set('n', '<leader>gb', ':Gblame<CR>', { desc = 'Git Blame' })
   end,
 }
