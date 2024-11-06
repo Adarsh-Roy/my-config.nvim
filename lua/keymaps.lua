6171718a5efc3f2fc6d434b8e681b73c4aafb465
Adarsh-Roy <79192126+Adarsh-Roy@users.noreply.github.com>
2024-11-06 10:20:37 +0530

    add window resizing keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -27,7 +27,7 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 -- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 -- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
--- Keybinds to make split navigation easier.
+-- Window Keymaps
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
@@ -36,6 +36,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<leader>-', '<C-w>-', { desc = 'Decrease window height' })
+vim.keymap.set('n', '<leader>+', '<C-w>+', { desc = 'Increase window height' })
+vim.keymap.set('n', '<leader><', '<C-w><', { desc = 'Decrease window width' })
+vim.keymap.set('n', '<leader>>', '<C-w>>', { desc = 'Increase window width' })
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
