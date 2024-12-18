5567177766e14a3487bedf4d7680586ec235b7b8
Adarsh-Roy <79192126+Adarsh-Roy@users.noreply.github.com>
2024-12-18 14:35:05 +0530

    change flash keymap to not contradict with surround and switch to error icons

--- lua/custom/plugins/flash.lua
+++ lua/custom/plugins/flash.lua
@@ -5,7 +5,7 @@ return {
   opts = {},
   -- stylua: ignore
   keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "<M-s>", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "<M-S>", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
   },
 }
