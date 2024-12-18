d8c7b0d9ade0b0b52c2ba6b94dd7238bac4653e7
Adarsh-Roy <79192126+Adarsh-Roy@users.noreply.github.com>
2024-12-18 15:26:23 +0530

    minor fix

--- init.lua
+++ init.lua
@@ -14,10 +14,9 @@ require 'lazy-plugins'
 -- set the current terminals title as the root_dir
 vim.api.nvim_create_autocmd('VimEnter', {
   callback = function()
-    local root_dir = vim.fn.fnamemodify(vim.fn.getcwd(), ':t') -- Get the current root folder name
+    local root_dir = vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
     vim.opt.title = true
     vim.opt.titlestring = root_dir
   end,
 })
--- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
