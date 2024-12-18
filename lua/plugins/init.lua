b344c54032e9b480289dd1d8385b4f261807f732
Adarsh-Roy <79192126+Adarsh-Roy@users.noreply.github.com>
2024-12-18 15:20:30 +0530

    refactor to move away from kickstart like directory structure

---
+++ lua/plugins/init.lua
@@ -0,0 +1,26 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  require 'plugins.oil',
+  require 'plugins.vim-fugitive',
+  require 'plugins.gitsigns',
+  require 'plugins.which-key',
+  require 'plugins.telescope',
+  require 'plugins.lspconfig',
+  require 'plugins.conform',
+  require 'plugins.cmp',
+  require 'plugins.tokyonight',
+  require 'plugins.todo-comments',
+  require 'plugins.mini',
+  require 'plugins.treesitter',
+  require 'plugins.indent_line',
+  require 'plugins.lint',
+  require 'plugins.autopairs',
+  require 'plugins.neo-tree',
+  require 'plugins.snacks',
+  require 'plugins.competiTest',
+  require 'plugins.flash',
+  require 'plugins.pyrightimport',
+}
