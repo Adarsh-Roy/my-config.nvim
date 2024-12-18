dc41f0fc649a00ae1128e669d95eb780ce067026
Adarsh-Roy <79192126+Adarsh-Roy@users.noreply.github.com>
2024-12-18 17:54:51 +0530

    random

--- init.lua
+++ init.lua
@@ -4,11 +4,8 @@ vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
 
 require 'options'
-
 require 'keymaps'
-
 require 'lazy-bootstrap'
-
 require 'lazy-plugins'
 
 -- set the current terminals title as the root_dir
